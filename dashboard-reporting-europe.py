a5d1e2b526b791fd7afb68ce76fd39d966c32ed2
Pierlou <pierlou.ramade@data.gouv.fr>
2024-11-07 16:17:59 +0100

    refactor: enable copying query and remove distributions for now

--- dashboard-reporting-europe.py
+++ dashboard-reporting-europe.py
@@ -59,12 +59,25 @@ def query_to_df(query, loop=True):
     return df
 
 
-resources_query = """prefix dct: <http://purl.org/dc/terms/>
+catalog_query = """prefix dcat:  <http://www.w3.org/ns/dcat#>
+prefix r5r: <http://data.europa.eu/r5r/>
+prefix dct: <http://purl.org/dc/terms/>
+
+select distinct ?catalog ?catalog_title
+where {
+?catalog dcat:dataset ?d.
+?d r5r:applicableLegislation <http://data.europa.eu/eli/reg_impl/2023/138/oj>.
+?catalog dct:title ?catalog_title.
+}
+LIMIT 20
+"""
+
+datasets_query = """prefix dct: <http://purl.org/dc/terms/>
 prefix r5r: <http://data.europa.eu/r5r/>
 prefix dcat:  <http://www.w3.org/ns/dcat#>
 
-select distinct ?d ?title ?cat_label ?license ?res_title ?accessURL ?downloadURL where {
-<http://data.europa.eu/88u/catalogue/plateforme-ouverte-des-donnees-publiques-francaises> ?cp ?d.
+select distinct ?d ?title ?cat_label where {
+<$CATALOG$> ?cp ?d.
 ?d r5r:applicableLegislation <http://data.europa.eu/eli/reg_impl/2023/138/oj>.
 ?d a dcat:Dataset.
 ?d dcat:distribution ?dist.
@@ -74,14 +87,32 @@ optional { ?d dct:title ?title.
 optional { ?d r5r:hvdCategory ?category. }
 OPTIONAL {?category skos:prefLabel ?cat_label. FILTER langMatches( lang(?cat_label),  "fr" )}
 ?d dct:publisher <$ORGA$>.
-optional { ?dist dcat:accessURL ?accessURL. }
-optional { ?dist dcat:downloadURL ?downloadURL. }
-optional { ?dist dct:title ?res_title.
-     FILTER ( langMatches( lang(?res_title),  "" ))
-}
-optional { ?dist dct:license ?license. }
 }
 """
+
+# resources_query = """prefix dct: <http://purl.org/dc/terms/>
+# prefix r5r: <http://data.europa.eu/r5r/>
+# prefix dcat:  <http://www.w3.org/ns/dcat#>
+
+# select distinct ?d ?title ?cat_label ?license ?res_title ?accessURL ?downloadURL where {
+# <$CATALOG$> ?cp ?d.
+# ?d r5r:applicableLegislation <http://data.europa.eu/eli/reg_impl/2023/138/oj>.
+# ?d a dcat:Dataset.
+# ?d dcat:distribution ?dist.
+# optional { ?d dct:title ?title.
+#      FILTER ( langMatches( lang(?title),  "" ))
+# }
+# optional { ?d r5r:hvdCategory ?category. }
+# OPTIONAL {?category skos:prefLabel ?cat_label. FILTER langMatches( lang(?cat_label),  "fr" )}
+# ?d dct:publisher <$ORGA$>.
+# optional { ?dist dcat:accessURL ?accessURL. }
+# optional { ?dist dcat:downloadURL ?downloadURL. }
+# optional { ?dist dct:title ?res_title.
+#      FILTER ( langMatches( lang(?res_title),  "" ))
+# }
+# optional { ?dist dct:license ?license. }
+# }
+# """
 
 
 def build_resource_link(resource_download_link):
@@ -105,6 +136,38 @@ app.layout = dbc.Container(
                     }),
         ]),
         dbc.Row([
+            html.H5('Sélectionner un catalogue source'),
+            dbc.Col([
+                dcc.Dropdown(
+                    id="catalog_dropdown",
+                    placeholder="data.gov.be, GovData...",
+                    value="http://data.europa.eu/88u/catalogue/plateforme-ouverte-des-donnees-publiques-francaises",
+                    clearable=True,
+                ),
+            ],
+                width=6,
+            ),
+            dbc.Col([
+                dcc.Clipboard(
+                    content=catalog_query,
+                    title="Copier la requête de récupération des catalogues",
+                    style={
+                        "display": "inline-block",
+                        "fontSize": 20,
+                        "verticalAlign": "top",
+                    },
+                ),
+            ],
+                width=2,
+            ),
+            dbc.Col([
+                dbc.Button(
+                    id='refresh_button',
+                    children='Rafraîchir les données'
+                ),
+            ],
+                width=4,
+            ),
             html.H5('Rechercher un producteur'),
             dbc.Col([
                 dcc.Dropdown(
@@ -112,13 +175,22 @@ app.layout = dbc.Container(
                     placeholder="Météo France, Shom...",
                     clearable=True,
                 ),
-            ]),
+            ],
+                width=8,
+            ),
             dbc.Col([
-                dbc.Button(
-                    id='refresh_button',
-                    children='Rafraîchir les données'
+                dcc.Clipboard(
+                    id="producteur_clipboard",
+                    title="Copier la requête de récupération des producteurs",
+                    style={
+                        "display": "inline-block",
+                        "fontSize": 20,
+                        "verticalAlign": "top",
+                    },
                 ),
-            ]),
+            ],
+                width=2,
+            ),
         ],
             style={"padding": "0px 0px 5px 0px"},
         ),
@@ -130,53 +202,93 @@ app.layout = dbc.Container(
 
 
 @app.callback(
-    Output('producteur_dropdown', 'options'),
+    Output('catalog_dropdown', 'options'),
     [Input('refresh_button', 'n_clicks')]
 )
-def resfresh_producteurs(click):
-    orgas = query_to_df("""prefix dct: <http://purl.org/dc/terms/>
+def resfresh_catalog(click):
+    catalog = query_to_df(catalog_query, loop=False)
+    return [{
+        "label": row["catalog_title"],
+        "value": row["catalog"],
+    } for _, row in catalog.iterrows()]
+
+
+@app.callback(
+    [
+        Output('producteur_dropdown', 'options'),
+        Output('producteur_dropdown', 'value'),
+        Output('producteur_clipboard', 'content'),
+    ],
+    [Input('catalog_dropdown', 'value')],
+)
+def resfresh_producteurs(catalog):
+    q = """prefix dct: <http://purl.org/dc/terms/>
     prefix r5r: <http://data.europa.eu/r5r/>
     prefix dcat:  <http://www.w3.org/ns/dcat#>
 
     select distinct ?pub_url ?orga where {
-    <http://data.europa.eu/88u/catalogue/plateforme-ouverte-des-donnees-publiques-francaises>  ?cp ?d.
+    <$CATALOG$>  ?cp ?d.
     ?d r5r:applicableLegislation <http://data.europa.eu/eli/reg_impl/2023/138/oj>.
     ?d dct:publisher ?pub_url.
     optional { ?pub_url foaf:name ?orga. }
     }
-    """)
+    """.replace("$CATALOG$", catalog)
+    orgas = query_to_df(q)
     return [{
         "label": row["orga"],
         "value": row["pub_url"],
-    } for _, row in orgas.iterrows()]
+    } for _, row in orgas.sort_values(by="orga").iterrows()], None, q
 
 
 @app.callback(
     Output('download_div', 'children'),
-    [Input('producteur_dropdown', 'value')]
-)
-def update_download_div(orga_url):
-    if not orga_url:
-        raise PreventUpdate
-    return [
-        dbc.Button(
-            id="download_csv_button",
-            children="Télécharger les données en csv",
-        ),
-        dcc.Download(id="download"),
+    [
+        Input('producteur_dropdown', 'value'),
+        Input('catalog_dropdown', 'value'),
     ]
+)
+def update_download_div(orga_url, catalog):
+    if not orga_url or not catalog:
+        return []
+    return dbc.Row([
+        dbc.Col([
+            dbc.Button(
+                id="download_csv_button",
+                children="Télécharger les données en csv",
+            ),
+            dcc.Download(id="download"),
+        ],
+            width=3,
+        ),
+        dbc.Col([
+            dcc.Clipboard(
+                content=datasets_query.replace("$ORGA$", orga_url).replace("$CATALOG$", catalog),
+                title="Copier la requête de récupération des jeux de données",
+                style={
+                    "display": "inline-block",
+                    "fontSize": 20,
+                    "verticalAlign": "top",
+                },
+            ),
+        ],
+            width=2,
+        ),
+    ])
 
 
 @app.callback(
     Output('download', 'data'),
     [Input('download_csv_button', 'n_clicks')],
-    [State('producteur_dropdown', 'value')],
+    [
+        State('producteur_dropdown', 'value'),
+        State('catalog_dropdown', 'value'),
+    ],
     prevent_initial_call=True,
 )
-def download_csv(click, orga_url):
+def download_csv(click, orga_url, catalog):
     if not orga_url:
         raise PreventUpdate
-    query = resources_query.replace("$ORGA$", orga_url)
+    query = datasets_query.replace("$ORGA$", orga_url).replace("$CATALOG$", catalog)
     url = (
         endpoint
         + "?default-graph-uri=&query="
@@ -188,55 +300,88 @@ def download_csv(click, orga_url):
 
 @app.callback(
     Output('loader', 'children'),
-    [Input('producteur_dropdown', 'value')]
+    [
+        Input('producteur_dropdown', 'value'),
+        Input('catalog_dropdown', 'value'),
+    ],
 )
-def update_markdown(orga_url):
-    if not orga_url:
-        raise PreventUpdate
-    query = resources_query.replace("$ORGA$", orga_url)
+def update_markdown(orga_url, catalog):
+    if not orga_url or not catalog:
+        return []
+    query = datasets_query.replace("$ORGA$", orga_url).replace("$CATALOG$", catalog)
     print(query)
-    resources = query_to_df(query, loop=True)
-    resources['resourceLink'] = resources['accessURL'].apply(build_resource_link)
-    markdown = (
-        f"##### [Lien vers les HVD de l'organisation sur data.gouv.fr]({orga_url + '?tag=hvd#/datasets'})\n"
-        f"#### {resources['d'].nunique()} "
-        f"jeu{'x' if resources['d'].nunique() > 1 else ''} de données HVD "
-        f"reporté{'s' if resources['d'].nunique() > 1 else ''} à l'Europe :\n"
+    datasets = query_to_df(query, loop=True)
+    markdown = ""
+    if "francaises" in catalog:
+        markdown += f"##### [Lien vers les HVD de l'organisation sur data.gouv.fr]({orga_url + '?tag=hvd#/datasets'})\n"
+    markdown += (
+        f"#### {len(datasets)} jeu{'x' if len(datasets) > 1 else ''} de données HVD "
+        f"reporté{'s' if len(datasets) > 1 else ''} à l'Europe :\n"
     )
-    for dataset in resources["title"].unique():
-        restr = resources.loc[resources["title"] == dataset]
-        dataset_url = restr["d"].unique()[0]
-        cat_label = ", ".join(restr["cat_label"].unique())
-        license = (
-            restr["license"].unique()[0]
-            if restr['license'].nunique() == 1
-            else 'plusieurs licences renseignées'
-        )
-        # without collapse
-        # markdown += f"- [{dataset}]({dataset_url}) ({len(restr)} ressource{'s' if len(restr) > 1 else ''})\n"
-        # for _, row in restr.iterrows():
-        #     markdown += f"   - [{row['res_title']}]({row['resourceLink']})\n"
-
-        # with collapse
-        markdown += (
-            f"###### [{dataset}]({dataset_url}) (catégorie{'s' if restr['cat_label'].nunique() > 1 else ''} `{cat_label}`, "
-            f"{licenses.get(license, license)})\n\n"
-            "<details>\n\n"
-            f"<summary>Voir {len(restr)} ressource{'s' if len(restr) > 1 else ''}</summary>\n\n"
-        )
-        for _, row in restr.iterrows():
-            if row['resourceLink']:
-                markdown += f"- [{row['res_title']}]({row['resourceLink']}) : {row['downloadURL']}\n"
-            else:
-                markdown += f"- {row['res_title']} /!\\ cette ressource n'existe plus\n"
-        markdown += "\n</details>\n\n"
+    for _, row in datasets.sort_values(by="title").iterrows():
+        markdown += f"- [{row['title']}]({row['d']}) (catégorie `{row['cat_label']}`)\n"
 
     # print(markdown)
     return dcc.Markdown(
         children=markdown,
         link_target="_blank",
-        dangerously_allow_html=True
+        # dangerously_allow_html=True
     )
+
+
+# retrieving resources is too slow, maybe we'll come back to this later
+# @app.callback(
+#     Output('loader', 'children'),
+#     [Input('producteur_dropdown', 'value')],
+#     [State('catalog_dropdown', 'value')],
+# )
+# def update_markdown(orga_url, catalog):
+#     if not orga_url:
+#         raise PreventUpdate
+#     query = resources_query.replace("$ORGA$", orga_url).replace("$CATALOG$", catalog)
+#     print(query)
+#     resources = query_to_df(query, loop=True)
+#     resources['resourceLink'] = resources['accessURL'].apply(build_resource_link)
+#     markdown = (
+#         f"##### [Lien vers les HVD de l'organisation sur data.gouv.fr]({orga_url + '?tag=hvd#/datasets'})\n"
+#         f"#### {resources['d'].nunique()} "
+#         f"jeu{'x' if resources['d'].nunique() > 1 else ''} de données HVD "
+#         f"reporté{'s' if resources['d'].nunique() > 1 else ''} à l'Europe :\n"
+#     )
+#     for dataset in resources["title"].unique():
+#         restr = resources.loc[resources["title"] == dataset]
+#         dataset_url = restr["d"].unique()[0]
+#         cat_label = ", ".join(restr["cat_label"].unique())
+#         license = (
+#             restr["license"].unique()[0]
+#             if restr['license'].nunique() == 1
+#             else 'plusieurs licences renseignées'
+#         )
+#         # without collapse
+#         # markdown += f"- [{dataset}]({dataset_url}) ({len(restr)} ressource{'s' if len(restr) > 1 else ''})\n"
+#         # for _, row in restr.iterrows():
+#         #     markdown += f"   - [{row['res_title']}]({row['resourceLink']})\n"
+
+#         # with collapse
+#         markdown += (
+#             f"###### [{dataset}]({dataset_url}) (catégorie{'s' if restr['cat_label'].nunique() > 1 else ''} `{cat_label}`, "
+#             f"{licenses.get(license, license)})\n\n"
+#             "<details>\n\n"
+#             f"<summary>Voir {len(restr)} ressource{'s' if len(restr) > 1 else ''}</summary>\n\n"
+#         )
+#         for _, row in restr.iterrows():
+#             if row['resourceLink']:
+#                 markdown += f"- [{row['res_title']}]({row['resourceLink']}) : {row['downloadURL']}\n"
+#             else:
+#                 markdown += f"- {row['res_title']} /!\\ cette ressource n'existe plus\n"
+#         markdown += "\n</details>\n\n"
+
+#     # print(markdown)
+#     return dcc.Markdown(
+#         children=markdown,
+#         link_target="_blank",
+#         dangerously_allow_html=True
+#     )
 
 
 # %%
